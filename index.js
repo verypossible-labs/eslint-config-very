57c9513f568ef7f97602c518e8714c7e20ac9849
gretzky <sfed617@gmail.com>
2021-06-04 13:35:27 -0400

    fix(extensions): extract extensions into their own file

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const jsExtensions = [".js", ".jsx", ".json"];
-const tsExtensions = [".ts", ".tsx", ".d.ts"];
-const extensions = [...jsExtensions, ...tsExtensions];
-
 module.exports = {
   env: {
     es6: true,
@@ -20,32 +16,4 @@ module.exports = {
     project: ["tsconfig.json"],
   },
   extends: ["./lib/core.js", "./lib/import.js", "./lib/format.js"],
-  settings: {
-    /**
-     * Tell import which file extensions to look for
-     * @see https://github.com/benmosher/eslint-plugin-import#importextensions
-     */
-    "import/extensions": extensions,
-    /**
-     * Tell import which files to look for
-     * @see https://github.com/benmosher/eslint-plugin-import#resolvers
-     */
-    "import/resolver": {
-      node: {
-        extensions: extensions,
-      },
-      /**
-       * Enable babel-plugin-module-resolver for resolving aliases
-       * @see https://github.com/tleunen/babel-plugin-module-resolver
-       */
-      "babel-module": {},
-    },
-    /**
-     * Tell import to use typescript-eslint
-     * @see https://github.com/benmosher/eslint-plugin-import#importparsers
-     */
-    "import/parsers": {
-      "@typescript-eslint/parser": tsExtensions,
-    },
-  },
 };
